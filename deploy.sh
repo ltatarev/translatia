d522e1c1d331f1fde2f2363ebb4975f7a976fa9b
ltatarev <lucija.tatarevic@gmail.com>
2022-02-09 01:32:08 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:ltatarev/translatia.git main:gh-pages
 
 cd -
